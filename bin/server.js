b73512dbed2e7023b2707adec1d7b3d1e48de7c8
Satoshi Kobayashi <koba@yk.rim.or.jp>
2022-11-16 22:42:06 +0800

    port と docs を指定できるよう修正

--- bin/server.js
+++ bin/server.js
@@ -2,16 +2,27 @@
 
 "use strict";
 
+const path  = require('path');
+
+const yargs = require('yargs');
+const argv = yargs
+    .usage('Usage: $0 <docs>')
+    .option('port', { alias: 'p', default: 4615 })
+    .demandCommand(0)
+    .argv;
+const port = argv.port;
+const docs = argv._[0] && path.resolve(argv._[0]);
+
 const express = require('express');
 
 const app = express();
-
-app.use(express.static('./'));
+if (docs) app.use(express.static(docs));
+app.use((req, res)=>res.status(404).send('<h1>Not Found</h1>'));
 
 const http = require('http').createServer(app);
 
-http.listen(4615, ()=>{
-    console.log('Server start on http://127.0.0.1:4615');
+http.listen(port, ()=>{
+    console.log(`Server start on http://127.0.0.1:${port}`);
 }).on('error', (e)=>{
     console.log('' + e);
     process.exit(-1);
