3e554c3a1b5c7295ad105f15349ade59021c6ce8
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-09-02 22:46:35 +0800

    ボット起動を関数化した

--- bin/proxy.js
+++ bin/proxy.js
@@ -8,6 +8,30 @@ const agent = 'mjai-proxy/' + version.replace(/^(\d+\.\d+).*$/,'$1');
 const net       = require('net');
 const { spawn } = require('child_process');
 
+function exec_bot() {
+
+    const server = net.createServer((sock)=>{
+
+        let reply = { type: 'hello', protocol: 'mjsonp', protocol_version: 3 };
+        if (argv.verbose) console.log('<-', reply);
+        sock.write(JSON.stringify(reply) + '\n');
+
+        sock.on('data', (data)=>{
+            let msg = JSON.parse(data.toString('utf-8'));
+            if (argv.verbose) console.log('->', msg);
+        });
+    }).listen(()=>{
+
+        const port = server.address().port;
+
+        spawn(bot, [`mjsonp://127.0.0.1:${port}/${room}`])
+            .on('error', (e)=>{
+                console.error(e.toString());
+                process.exit(-1);
+            });
+    });
+}
+
 const argv = require('yargs')
     .usage('Usage: $0 server-url mjai-bot -- [ bot-params... ]')
     .option('name',     { alias: 'n', default: 'Mjaiボット'})
@@ -19,25 +43,6 @@ const argv = require('yargs')
 const url  = argv._[0] == '-' ? 'http://127.0.0.1:4615/server'
                               : ('' + argv._[0]).replace(/\/$/,'');
 const room = argv.room;
+const bot  = argv._[1];
 
-const server = net.createServer((sock)=>{
-
-    let reply = { type: 'hello', protocol: 'mjsonp', protocol_version: 3 };
-    if (argv.verbose) console.log('<-', reply);
-    sock.write(JSON.stringify(reply) + '\n');
-
-    sock.on('data', (data)=>{
-        let msg = JSON.parse(data.toString('utf-8'));
-        if (argv.verbose) console.log('->', msg);
-    });
-
-}).listen(0, ()=>{
-
-    const port = server.address().port;
-    const bot = spawn(argv._[1], [`mjsonp://127.0.0.1:${port}/${room}`]);
-
-    bot.on('error', (e)=>{
-        console.error(e.toString());
-        process.exit(-1);
-    });
-});
+exec_bot();
