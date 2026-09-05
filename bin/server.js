d2820e55484c30cfd93b752b66a0c80fc128955e
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-09-06 05:32:52 +0800

    麻雀サーバーに局の進行速度を調整するオプション --speed を仮追加

--- bin/server.js
+++ bin/server.js
@@ -15,6 +15,7 @@ const argv = yargs
     .option('oauth',    { alias: 'o' })
     .option('store',    { alias: 's' })
     .option('status',   { alias: 'S', boolean: true })
+    .option('speed',    {             default: 2    })
     .argv;
 const port = argv.port;
 const base = ('' + argv.baseurl)
@@ -45,7 +46,7 @@ const app  = express();
 const http = require('http').createServer(app);
 const io   = require('socket.io')(http, { path: `${base}/socket.io/` });
 
-const lobby = require('../lib/lobby')(io);
+const lobby = require('../lib/lobby')(io, argv.speed);
 
 app.use(session);
 app.use(passport.initialize());
